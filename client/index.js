daca1ac4aaae8688158e0a9ac97aab1470236256
shadow567 <shadow567@yakoo.com>
2024-08-11 17:52:08 -0700

    solution

--- client/index.js
+++ client/index.js
@@ -6,12 +6,19 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  const MERKLE_ROOT = merkleTree.getRoot();
+  let index = 0;
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof: merkleTree.getProof(index, layer = merkleTree.leaves),
+    name: niceList[index],
   });
 
+  console.log(niceList[index]);
   console.log({ gift });
+
 }
 
 main();
